a854ae2cbc718083b10c1f0cb374326a27bfa013
ChenglingL <chengling.li@emory.edu>
2024-01-23 20:46:11 -0600

    p3.80 from tau alpha 10 to 1000 simulations on NCSADelta

---
+++ glassyDynamicsProject/submissionScript/multipleJobsCPU.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#jobs on the CPU -- for n=4096, I estimate that if tau_alpha <= 10^(10/3) then it will finish within the 48 hour maxJobTime
+#jobs on the CPU -- for n=32768, I estimate that if tau_alpha <= 10^(7/3) then it will finish within the 48 hour maxJobTime
+number=4096
+p=3.8
+
+eqWaitMultiple=100.
+nRelaxations=10.
+
+ tauEstimate=(10. 21.54 46.41 100. 215.44 464.15 1000.)
+# temperatures=(0.08868 0.05586 0.03756 0.02652 0.01945 0.01471 0.01141)
+
+temperatures=(0.05409 0.03105 0.01782 0.01023 0.005873 0.003371 0.001935)
+
+records=(0 1 2)
+
+for recordIdx in ${records[@]}; do
+    for i in ${!tauEstimate[@]}; do
+        tauEst=${tauEstimate[$i]}
+        temp=${temperatures[$i]}
+        echo ${number} ${p} ${temp} ${tauEst} ${eqWaitMultiple} ${nRelaxations}  ${recordIdx}
+        sbatch /u/cli6/cellGPU/glassyDynamicsProject/submissionScript/baseSubmitCPU.sh ${number} ${p} ${temp} ${tauEst} ${eqWaitMultiple} ${nRelaxations} ${recordIdx}
+    done
+done
+
+
