8c98bc5dcfa75c66395b81688dc8df490fcac0a0
ChenglingL <chenglingli0202@gmail.com>
2024-04-09 12:56:33 -0400

    reform of directory tree

---
+++ cmakeHelp/CMakeFiles/CRMSDProductionRuns.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../localTest/executable/CRMSDProductionRuns.out"
+  "../localTest/executable/CRMSDProductionRuns.out.pdb"
+  "CMakeFiles/CRMSDProductionRuns.out.dir/localTest/CRMSDProductionRuns.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CRMSDProductionRuns.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- glassyDnamicsProject/readme.txt
+++
@@ -1,1 +0,0 @@
-This folder is for T-dependent mechanical response.
