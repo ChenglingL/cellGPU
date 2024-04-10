63c771f2d7d18e0f396e96377b624225ebf3567c
ChenglingL <chenglingli0202@gmail.com>
2024-04-10 19:01:57 -0400

    start to measure the trace of stress tensor

---
+++ cmakeHelp/CMakeFiles/MSDProductionRuns.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../localTest/executable/MSDProductionRuns.out"
+  "../localTest/executable/MSDProductionRuns.out.pdb"
+  "CMakeFiles/MSDProductionRuns.out.dir/localTest/MSDProductionRuns.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MSDProductionRuns.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
