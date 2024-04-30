c2efd9697b7a4865818f557d6f7417e65d85dcbe
ChenglingL <chenglingli0202@gmail.com>
2024-04-30 16:14:53 -0400

    test with larger epsilon

--- glassyDynamicsProject/submissionScript/multiPureShear.sh
+++ glassyDynamicsProject/submissionScript/multiPureShear.sh
@@ -9,7 +9,7 @@ temperatures=(0.063 0.039 0.03105 0.016 0.01 0.008 0.0063 0.005 0.00385 0.0031 0
 records=(10)
 
 tauEstimate=(1. 2. 4. 9. 15. 25. 40. 60. 80. 120. 200. 300. 1000.)
-epsilons=(0.0001 -0.0001)
+epsilons=(0.05 -0.05)
 
 for recordIdx in ${records[@]}; do
     for epsilon in ${epsilons[@]}; do
@@ -21,6 +21,25 @@ for recordIdx in ${records[@]}; do
         done
     done
 done
+# number=4096
+# p=3.825
+
+# temperatures=(0.063 0.039 0.03105 0.016 0.01 0.008 0.0063 0.005 0.00385 0.0031 0.0025 0.002 0.0015)
+# records=(10)
+
+# tauEstimate=(1. 2. 4. 9. 15. 25. 40. 60. 80. 120. 200. 300. 1000.)
+# epsilons=(0.0001 -0.0001)
+
+# for recordIdx in ${records[@]}; do
+#     for epsilon in ${epsilons[@]}; do
+#         for i in ${!temperatures[@]}; do
+#             tauEst=${tauEstimate[$i]}
+#             temp=${temperatures[$i]}
+#             echo ${number} ${p} ${temp} ${recordIdx} ${tauEst} ${epsilon}
+#             sbatch /u/cli6/cellGPU/glassyDynamicsProject/submissionScript/singlePureShearCPU.sh ${number} ${p} ${temp} ${recordIdx} ${tauEst} ${epsilon}
+#         done
+#     done
+# done
 
 
 # number=4096
